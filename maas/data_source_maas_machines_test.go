3dfb298894b48b73fcd3b80dba78fe69a5c4a88a
Ricard Bejarano <ricard@bejarano.io>
2025-04-14 15:38:32 +0200

    docs: added comment about MAAS Machines API returning machines sorted by id

--- maas/data_source_maas_machines_test.go
+++ maas/data_source_maas_machines_test.go
@@ -24,6 +24,7 @@ func TestAccDataSourceMAASMachines_basic(t *testing.T) {
 			if err := resource.TestCheckResourceAttr("data.maas_machines.test", "machines.#", strconv.Itoa(len(machines)))(s); err != nil {
 				return err
 			}
+			// According to MAAS' API documentation, machines are returned "sorted by id (i.e. most recent last)"
 			for i, machine := range machines {
 				if err := resource.TestCheckResourceAttr("data.maas_machines.test", fmt.Sprintf("machines.%v.system_id", i), machine.SystemID)(s); err != nil {
 					return err
