19f41fce4caa84510299de0320ffb3b2b9ff2119
Ricard Bejarano <ricard@bejarano.io>
2025-04-16 10:24:34 +0200

    test: refactored data.maas_machines test not to use the MAAS API

--- maas/data_source_maas_machines_test.go
+++ maas/data_source_maas_machines_test.go
@@ -3,7 +3,6 @@ package maas_test
 import (
 	"fmt"
 	"strconv"
-	"terraform-provider-maas/maas"
 	"terraform-provider-maas/maas/testutils"
 	"testing"
 
@@ -14,26 +13,21 @@ import (
 func TestAccDataSourceMAASMachines_basic(t *testing.T) {
 	checks := []resource.TestCheckFunc{
 		func(s *terraform.State) error {
-			conn := testutils.TestAccProvider.Meta().(*maas.ClientConfig).Client
+			rs, ok := s.RootModule().Resources["data.maas_machines.test"]
+			if !ok {
+				return fmt.Errorf("data source not found: data.maas_machines.test")
+			}
 
-			machines, err := conn.Machines.Get(nil)
+			n, err := strconv.Atoi(rs.Primary.Attributes["machines.#"])
 			if err != nil {
 				return err
 			}
 
-			// We're about to compare the `machines` we just got, with the `machines` data source attribute.
-			// There's a slim chance that the upstream MAAS API list of machines changed in between both times we read.
-			// If any of the following tests fail for no apparent reason, that might explain why: a change might've gone through in between them.
-			// Try re-running the tests if you suspect that's what's happening.
-			if err := resource.TestCheckResourceAttr("data.maas_machines.test", "machines.#", strconv.Itoa(len(machines)))(s); err != nil {
-				return err
-			}
-			// According to MAAS' API documentation, machines are returned "sorted by id (i.e. most recent last)"
-			for i, machine := range machines {
-				if err := resource.TestCheckResourceAttr("data.maas_machines.test", fmt.Sprintf("machines.%v.system_id", i), machine.SystemID)(s); err != nil {
+			for i := 0; i < n; i++ {
+				if err := resource.TestCheckResourceAttrSet("data.maas_machines.test", fmt.Sprintf("machines.%v.system_id", i))(s); err != nil {
 					return err
 				}
-				if err := resource.TestCheckResourceAttr("data.maas_machines.test", fmt.Sprintf("machines.%v.hostname", i), machine.Hostname)(s); err != nil {
+				if err := resource.TestCheckResourceAttrSet("data.maas_machines.test", fmt.Sprintf("machines.%v.hostname", i))(s); err != nil {
 					return err
 				}
 			}
